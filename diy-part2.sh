56eec4dc1df35e46630f030ecb09b3dc4d13c91f
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-10 20:58:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,8 @@ git clone https://github.com/GruntFish/OP-Packages ./package/OP-Packages
 git clone https://github.com/sirpdboy/luci-app-lucky ./package/luci-app-lucky
 git clone https://github.com/sirpdboy/luci-app-watchdog ./package/luci-app-watchdog
 
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom ./package/luci-theme-infinityfreedom
+# 启用的话添加到config      luci-theme-infinityfreedom-ng=y
+# git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom ./package/luci-theme-infinityfreedom-ng
 
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
