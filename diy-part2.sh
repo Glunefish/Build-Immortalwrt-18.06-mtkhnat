96651c5924599110d8922f0e628b91bffba5b8a2
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-10 12:18:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,20 +10,22 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+ls
 
 rm -rf feeds/packages/net/adguardhome
     
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
+ls package/
 
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom ./package/luci-theme-infinityfreedom
-git clone https://github.com/lwb1978/openwrt-gecoosac ./package/openwrt-gecoosac
-git clone https://github.com/zzsj0928/luci-app-pushbot ./package/luci-app-pushbot
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 
-git clone https://github.com/GruntFish/OP-Packages ./package/OP-Packages
-git clone https://github.com/sirpdboy/luci-app-lucky ./package/luci-app-lucky
-git clone https://github.com/sirpdboy/luci-app-watchdog ./package/luci-app-watchdog
+git clone https://github.com/GruntFish/OP-Packages package/OP-Packages
+git clone https://github.com/sirpdboy/luci-app-lucky package/luci-app-lucky
+git clone https://github.com/sirpdboy/luci-app-watchdog package/luci-app-watchdog
 
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
