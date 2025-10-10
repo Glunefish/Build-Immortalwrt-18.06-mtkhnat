25274d241be8499dc9515288c72adfd40547d1c2
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-10 11:42:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,3 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# sed -i 's/KERNEL_PATCHVER:=6.12/KERNEL_PATCHVER:=6.6/' target/linux/qualcommax/Makefile
-
-git clone https://github.com/lwb1978/openwrt-gecoosac ./package/openwrt-gecoosac
-git clone https://github.com/kenzok78/luci-app-adguardhome ./package/luci-app-adguardhome
