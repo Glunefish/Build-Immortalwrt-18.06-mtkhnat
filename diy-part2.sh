6d538a881f552248c0885959c4b8a24241023edb
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-10 18:36:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,12 +11,12 @@
 #
 
 rm -rf feeds/packages/net/adguardhome
-    
+
+# 更新 golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom ./package/luci-theme-infinityfreedom
 git clone https://github.com/lwb1978/openwrt-gecoosac ./package/openwrt-gecoosac
 git clone https://github.com/zzsj0928/luci-app-pushbot ./package/luci-app-pushbot
 
@@ -24,6 +24,7 @@ git clone https://github.com/GruntFish/OP-Packages ./package/OP-Packages
 git clone https://github.com/sirpdboy/luci-app-lucky ./package/luci-app-lucky
 git clone https://github.com/sirpdboy/luci-app-watchdog ./package/luci-app-watchdog
 
+# git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom ./package/luci-theme-infinityfreedom
 
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
