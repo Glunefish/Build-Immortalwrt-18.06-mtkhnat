1a09220fbca89331dfbc447530247f6005fa1b71
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-11 19:51:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,19 +10,12 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-rm -rf ./feeds/packages/net/adguardhome
-git clone -b master --single-branch --depth=1 https://github.com/coolsnowwolf/packages ./ledepackage
-sed -i 's|include \.\./\.\./lang/golang/golang-package\.mk|include \$(TOPDIR)/feeds/packages/lang/golang/golang-package.mk|g' ./ledepackage/net/adguardhome/Makefile
-mv ./ledepackage/net/adguardhome ./feeds/packages/net/adguardhome
-rm -rf ./ledepackage
-
-
-
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
+rm -rf ./feeds/packages/net/adguardhome
+git clone https://github.com/GruntFish/openwrt-adguardhome ./package/openwrt-adguardhome 
 
-git clone https://github.com/GruntFish/luci-app-adguardhome ./package/luci-app-adguardhome
 git clone https://github.com/lwb1978/openwrt-gecoosac ./package/openwrt-gecoosac
 git clone https://github.com/zzsj0928/luci-app-pushbot ./package/luci-app-pushbot
 git clone https://github.com/sirpdboy/luci-app-lucky ./package/luci-app-lucky
