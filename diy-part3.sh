172bf778ddefe3b891a34cc51422b349cfe1c97f
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-12 14:09:19 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -12,6 +12,6 @@
 
 sed -i 's/0x2B2/0x312/g' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
 
-sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/' feeds/luci/collections/luci/Makefile
+sed -i 's/Bootstrap theme/infinityfreedom theme/g' feeds/luci/collections/luci/Makefile
 
 sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
