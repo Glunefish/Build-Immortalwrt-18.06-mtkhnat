0e24128e6cb5533c01925f8c400ad011e0d7833c
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-12 15:38:34 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -12,6 +12,9 @@
 
 sed -i 's/0x2B2/0x312/g' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
 
-sed -i 's/Bootstrap theme/infinityfreedom theme/g' feeds/luci/collections/luci/Makefile
+sed -i "s|option mediaurlbase /luci-static/bootstrap|option mediaurlbase /luci-static/infinityfreedom|" feeds/luci/modules/luci-base/root/etc/config/luci
 
 sed -i 's/192.168.1.1/192.168.10.254/g' package/base-files/files/bin/config_generate
+
+NEW_PASSWORD_HASH=$(openssl passwd -1 "admin.123")
+sed -i "s|root::0:0:99999:7:::|root:${NEW_PASSWORD_HASH}:0:0:99999:7:::|" package/base-files/files/etc/shadow
