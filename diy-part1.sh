ab1dd04ce046f37923ea94f383c7a31171912ba4
GruntFish <165454237+GruntFish@users.noreply.github.com>
2025-10-14 14:47:51 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,5 @@
 
 # echo 'src-git full https://github.com/immortalwrt/packages.git;openwrt-23.05' >> feeds.conf.default
 # echo 'src-git luci https://github.com/immortalwrt/luci.git;openwrt-23.05' >> feeds.conf.default
+
+echo 'https://github.com/GruntFish/GPackages;Package' >> feed.conf.default
